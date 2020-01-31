aec1cc537c7e6de2618c87b26b1b8a9a63a35aba
parashuram-patil <parasharam_patil@ymail.com>
2020-01-31 18:21:35 +0530

    Using SES - sends only if both emails are verified in SES - https://docs.aws.amazon.com/ses/latest/DeveloperGuide/request-production-access.html

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@
 
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3()
+var ses = new AWS.SES({ region: 'eu-west-1' });
 
 module.exports.hello = async event => {
   return {
@@ -25,4 +26,32 @@ module.exports.listBuckets = async event => {
     })
   }
   );
+}
+
+module.exports.sendEmail = async event => {
+  const params = {
+    Source: "parshuram.patil@outlook.in",
+    Destination: {
+      ToAddresses: ["parasharam_patil@ymail.com"],
+    },
+    Message: {
+      Subject: {
+        Data: "SES Test",
+        Charset: "UTF8"
+      },
+      Body: {
+        Text: {
+          Data: "Hi,\nThis a test mail using SES",
+          Charset: "UTF8"
+        },
+      },
+    },
+  }
+
+  ses.sendEmail(params, function (err, res) {
+    if (err)
+      console.log("Error ---> " + err.message)
+
+    console.log("Success ---> " + res.MessageId);
+  })
 }
